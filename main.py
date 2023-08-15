c0671d8a810db30953c73fa405a769defddc36f9
dionzand <dion.zandstra@gmail.com>
2023-08-15 15:44:16 +0200

    init

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
+import re
+
 import streamlit as st
 
-st.text_area("Paste your sequence here", height=100, key="seq")
+input_seq = st.text_area("Paste your sequence here", height=100, key="seq")
+min_repeats = st.number_input("Minimum number of repeats", min_value=1, value=3, step=1, key="min_repeats")
+
+motifs_list = [line.rstrip('\n') for line in open("motifs.txt")]
+base_motifs_list = [line.rstrip("\n").split(",") for line in open("motifs_dict.txt")]
+base_motifs_dict = {key: value for key, value in base_motifs_list}
+
+str_repeat_list = []
+
+for str_motif in motifs_list:
+    if str_motif * min_repeats in input_seq:
+        pattern = f"((?:{str_motif}){{{min_repeats},}})"
+
+        for match in re.finditer(pattern, input_seq):
+            str_repeat_list.append({"start": match.start(1), "end": match.end(1), "motif": str_motif,
+                                    "repeats": match.group(1).count(str_motif)})
+    else:
+        continue
+
+sorted_str_repeat_list = sorted(str_repeat_list, key=lambda d: d['start'])
+
+nomenclature = "[START]"
+cursor = 0
+
+for motif in sorted_str_repeat_list:
+    if motif["start"] == cursor:
+        nomenclature += f"[{base_motifs_dict[motif['motif']]}]{motif['repeats']}"
+        cursor = motif['end']
+    else:
+        nomenclature += f"[N]{motif['start'] - cursor}"
+        nomenclature += f"[{base_motifs_dict[motif['motif']]}]{motif['repeats']}"
+        cursor = motif['end']
+if cursor < len(input_seq):
+    nomenclature += f"[N]{len(input_seq) - cursor}"
+nomenclature += "[END]"
+
+st.write(nomenclature)
